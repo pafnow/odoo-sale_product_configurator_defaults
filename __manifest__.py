234ce7666c78d40b560862a0fc83797cfecb5df6
Pafnow <pafnow@hotmail.com>
2020-08-18 10:44:25 +0700

    add MRM in author manifests

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Sale Product Configurator Default Values',
     'version': '13.0.1.0.0',
     'depends': ['sale_product_configurator', 'product_attribute_defaults'],
-    'author': 'Pafnow',
+    'author': 'MRM, Pafnow',
     'category': 'Hidden',
     'description': 'Handle default values for attributes of product via the configurator',
     'data': [
